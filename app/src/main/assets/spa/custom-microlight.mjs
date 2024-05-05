0861e29eb2e75309de7a2d4083657d5bff7ccfda
Tom Shaver <l3l_aze@yahoo.com>
2024-05-04 23:14:09 -0500

    Add JS `string` support. Keywords as list.
    * Updated tokens/tokenizer to add ` string support. +1 to each token index beyond the 3x string types (just comment, xml comment, multiline comment yet).
    * Keywords from regexp as list in comments, for future use.

--- app/src/main/assets/spa/custom-microlight.mjs
+++ app/src/main/assets/spa/custom-microlight.mjs
@@ -30,10 +30,11 @@ function highlight (el, language, hashAsComment = false) {
   //  4: regex
   //  5: string starting with "
   //  6: string starting with '
-  //  7: xml comment  <!-- -->
-  //  8: multiline comment /* */
-  //  9: single-line comment starting with two slashes //
-  // 10: single-line comment starting with hash #
+  //  7: string starting with `
+  //  8: xml comment  <!-- -->
+  //  9: multiline comment /* */
+  // 10: single-line comment starting with two slashes //
+  // 11: single-line comment starting with hash #
   let tokenType = 0
 
   // kept to determine between regex and division
@@ -52,7 +53,7 @@ function highlight (el, language, hashAsComment = false) {
 
   // running through characters and highlighting
   /* eslint-disable-next-line no-sequences */
-  while (prev2 = prev1, prev1 = tokenType < 7 && prev1 === '\\' ? 1 : chr) {
+  while (prev2 = prev1, prev1 = tokenType < 8 && prev1 === '\\' ? 1 : chr) {
   // escaping if needed (with exception for comments), previous character
   // will not be therefore recognized as a token finalize condition
     chr = next1
@@ -61,8 +62,8 @@ function highlight (el, language, hashAsComment = false) {
 
     // check if current token should be finalized
     if (!chr || // end of content
-      // types 9-10 (single-line comments) end with a newline
-      (tokenType > 8 && chr === '\n') ||
+      // types 10-11 (single-line comments) end with a newline
+      (tokenType > 9 && chr === '\n') ||
       [ // finalize conditions for other token types
         /\S/[test](chr), // 0: whitespaces. merged together
         1, // 1: operators. consist of a single character
@@ -71,8 +72,9 @@ function highlight (el, language, hashAsComment = false) {
         (prev1 === '/' || prev1 === '\n') && multichar, // 4: regex
         prev1 === '"' && multichar, // 5: string with "
         prev1 === "'" && multichar, // 6: string with '
-        text[pos - 4] + prev2 + prev1 === '-->', // 7: xml comment
-        prev2 + prev1 === '*/' // 8: multiline comment
+        prev1 === '`' && multichar, // 7: string with `
+        text[pos - 4] + prev2 + prev1 === '-->', // 8: xml comment
+        prev2 + prev1 === '*/' // 9: multiline comment
       ][tokenType]
     ) {
       // appending the token to the result
@@ -82,12 +84,35 @@ function highlight (el, language, hashAsComment = false) {
             ? 0 // punctuation
             : tokenType < 3
               ? 2 // comments
-              : tokenType > 6
+              : tokenType > 7
                 ? 4 // regex and strings
                 : tokenType > 3
                   ? 3
                   // otherwise tokenType === 3, (key)word (with +, becomes 1 if regexp matches, or 0 otherwise)
                   : +/^(a(bstract|lias|nd|rguments|rray|s(m|sert)?|uto|sync|wait)|b(ase|egin|ool(ean)?|reak|yte)|c(ase|atch|har|hecked|lass|lone|ompl|on(sole|st(ructor)?)?|ontinue)|de(bugger|cimal|clare|f(ault|er)?|init|l(egate|ete)?)|do(cument|uble)|display|e(cho|ls?if|lse(if)?|nd|nsure|num|vent|x(cept|ec|p(licit|ort)|te(nds|nsion|rn)))|f(allthrough|alse|inal(ly)?|ixed|loat|or([eE]ach)?|ilter|riend|rom|unc(tion)?)|global|goto|guard|h(ead|ref)|i(f|mp(lements|licit|ort)|n(it|clude(s)?(_once)?|line|out|stanceof|t(erface|ernal)?)?|s|ndexOf)|l(ambda|e(ngth|t)|oc(ation|k)|ong|astIndexOf)|m(ap|icrolight|odule|utable)|NaN|n(amespace|ative|ext|ew|il|ot|ull)|o(bject|f|perator|r|ut|verride)|p(ackage|arams|rivate|rotected|rotocol|ublic|arentElement)|r(aise|e(adonly|do|f|gister|peat|quire(_once)?|scue|strict|try|turn|move(Child)?))|s(byte|ealed|elf|hort|igned|izeof|tatic|tring|truct|ubscript|uper|ynchronized|witch|lice|tyle)|t(emplate|hen|his|hrows?|ransient|rue|ry|ype(alias|def|id|name|of)|extContent)|u(n(checked|def(ined)?|ion|less|signed|til)|se|sing)|v(ar|irtual|oid|olatile)|w(char_t|hen|here|hile|i(ndow|th))|xor|yield)$/[test](token)
+
+          // abstract,alias,and,arguments,array,asm,assert,async,await,
+          // base,begin,bool,boolean,break,byte,
+          // case,catch,char,checked,class,clone,compl,console,constructor,continue,
+          // debugger,decimal,declare,def,fault,defer,delegate,delete,do,document,double,display,
+          // echo,elseif,else,ensure,enum,event,except,exec,explicit,export,extend,
+          // fallthrough,false,final,finally,fixed,float,foreach,filter,friend,from,function,
+          // global,goto,guard,
+          // head,href
+          // if,implements,implicit,import,include,includes,include_once,inline,instanceof,int,indexOf,
+          // lambda,length,let,location,lock,long,lastIndexOf,
+          // map,module,mutable,
+          // NaN,namespace,native,next,new,nil,not,null,
+          // object,of,operator,or,out,override,
+          // package,params,private,protected,protocol,public,parentElement,
+          // raise,readonly,redo,ref,register,repeat,require,require_once,rescue,restrict,return,remove,removeChild,
+          // sbyte,sealed,self,short,signed,sizeof,static,string,struct,subscript,super,synchronized,switch,slice,style,
+          // template,then,this,throw,throws,transient,true,try,type,typealias,typedef,typeid,typename,
+          // un,unchecked,undefined,union,unless,unsigned,until,use,using,
+          // var,virtual,void,volatile,
+          // wchar_t,when,where,while,window,width,
+          // xor,
+          // yield
         ]
 
         // remapping token type into style
@@ -119,7 +144,7 @@ function highlight (el, language, hashAsComment = false) {
 
       // saving the previous token type (skipping whitespaces and comments)
       lastTokenType =
-        (tokenType && tokenType < 7)
+        (tokenType && tokenType < 8)
           ? tokenType
           : lastTokenType
 
@@ -127,7 +152,7 @@ function highlight (el, language, hashAsComment = false) {
 
       // determining the new token type (going up the
       // list until matching a token type start condition)
-      tokenType = 11
+      tokenType = 12
       while (![
         1, //  0: whitespace
         /[/{}[(\-+*=<>:|\\.,?!&@~;$]/[test](chr) || (chr === '#' && !hashAsComment), //  1: operator or braces
@@ -138,16 +163,17 @@ function highlight (el, language, hashAsComment = false) {
         prev1 !== '<', // workaround for xml closing tags
         chr === '"', //  5: string with "
         chr === "'", //  6: string with '
-        // chr === '`' // 7: string with `
-        chr + next1 + text[pos + 1] + text[pos + 2] === '<!--', //  7: xml comment
-        chr + next1 === '/*', //  8: multiline comment
-        chr + next1 === '//', //  9: single-line comment
-        chr === '#' && hashAsComment // 10: hash-style comment
+        chr === '`', // 7: string with `
+        chr + next1 + text[pos + 1] + text[pos + 2] === '<!--', //  8: xml comment
+        chr + next1 === '/*', //  9: multiline comment
+        chr + next1 === '//', // 10: single-line comment
+        chr === '#' && hashAsComment // 11: hash-style comment
       ][--tokenType]);
     }
 
     if (chr === '\n') {
-      codeLine[appendChild](_document.createTextNode('\n')) // to preserve newlines during copy
+      // Preserve newlines in text representation.
+      codeLine[appendChild](_document.createTextNode('\n'))
 
       el[appendChild](codeLine)
 
